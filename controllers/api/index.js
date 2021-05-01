c27eee0d5d89d2c5c90ac6df375a7422286bb96b
srad25 <suzanne.radlein@gmail.com>
2021-05-01 11:13:13 -0400

    adding end to end pipeline for commboard

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const ownerRoutes = require('./ownerRoutes');
+const commentRoutes = require('./commentRoutes');
 
 router.use('/owners', ownerRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const Owner = require('./Owner');
 const Request = require('./Request');
+const Comment = require('./Comment')
 
 Owner.hasMany(Request, {
   foreignKey: 'owner_id',
@@ -10,4 +11,22 @@ Request.belongsTo(Owner, {
   foreignKey: 'owner_id'
 });
 
-module.exports = { Owner, Request };
+Owner.hasMany(Comment, {
+  foreignKey: 'owner_id'
+});
+
+Comment.belongsTo(Owner, {
+  foreignKey: 'owner_id'
+});
+
+Request.hasMany(Comment, {
+  foreignKey: 'post_id',
+  allowNull: true
+});
+
+Comment.belongsTo(Request, {
+  foreignKey: 'post_id',
+});
+
+module.exports = { Owner, Request, Comment };
+
