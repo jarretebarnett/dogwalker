d26433e0b4eaaf219629f5b5c86a9938c21d06c5
srad25 <suzanne.radlein@gmail.com>
2021-05-02 03:37:13 -0400

    working on css

--- models/Comment.js
+++ models/Comment.js
@@ -30,7 +30,7 @@ Comment.init(
     post_id: {
         type: DataTypes.INTEGER,
         references: {
-            model: 'request',
+            model: 'owner',
             key: 'id'
         }
     }
