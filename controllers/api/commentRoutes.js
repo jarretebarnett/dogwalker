8925ddc2c3c7a06d383ae7aa5f316264caa4d233
srad25 <suzanne.radlein@gmail.com>
2021-05-03 03:10:43 -0400

    fixing bug in comment

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -2,7 +2,51 @@ const router = require('express').Router();
 const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.post('/commBoard', withAuth, async (req, res) => {
+
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newComment = await Comment.create({
+      ...req.body,
+      owner_id: req.session.owner_id,
+    });
+
+    res.status(200).json(newComment);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const commentData = await Comment.destroy({
+      where: {
+        id: req.params.id,
+        owner_id: req.session.owner_id,
+      },
+    });
+
+    if (!commentData) {
+      res.status(404).json({ message: 'No comment found with this id!' });
+      return;
+    }
+
+    res.status(200).json(commentData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
+
+
+
+
+
+
+
+
+
+/*router.post('/commBoard', withAuth, async (req, res) => {
     try {
       const newPost = await Comment.create({
         ...req.body,
@@ -18,4 +62,4 @@ router.post('/commBoard', withAuth, async (req, res) => {
   });
 
   
-module.exports = router;
+module.exports = router;*/
--- models/index.js
+++ models/index.js
@@ -2,31 +2,16 @@ const Owner = require('./Owner');
 const Request = require('./Request');
 const Comment = require('./Comment')
 
-Owner.hasMany(Request, {
+Owner.hasMany(Comment, {
   foreignKey: 'owner_id',
   onDelete: 'CASCADE'
-});
-
-Request.belongsTo(Owner, {
-  foreignKey: 'owner_id'
-});
-
-Owner.hasMany(Comment, {
-  foreignKey: 'owner_id'
 });
 
 Comment.belongsTo(Owner, {
   foreignKey: 'owner_id'
 });
 
-Request.hasMany(Comment, {
-  foreignKey: 'post_id',
-  allowNull: true
-});
 
-Comment.belongsTo(Request, {
-  foreignKey: 'post_id',
-});
 
 module.exports = { Owner, Request, Comment };
 
